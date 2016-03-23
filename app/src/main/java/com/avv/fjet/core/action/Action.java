56da8fe8cabf4a1b40d721deabeccadc2a5c80bc
Alexander <sandor006400@gmail.com>
2016-03-23 23:09:55 +0100

    + Accion llama a sus nuevos Observers

--- app/src/main/java/com/avv/fjet/core/action/Action.java
+++ app/src/main/java/com/avv/fjet/core/action/Action.java
@@ -1,16 +1,88 @@
 package com.avv.fjet.core.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Creado por Alexander Vladimirovich Vorobiev / 27/02/2016
+ * Created by Alexander Vladimirovich Vorobiev
+ * At 23/03/2016
  */
 public abstract class Action {
 
-    public abstract void execute();
+    // region - Constants
 
-    public abstract void undo();
+    // endregion - Constants
 
-    interface IActionData {
+    // region - Fields
+
+    private List<ActionObserver> obsevers = new ArrayList<>();
+
+    // endregion - Fields
+
+    // region - Constructors
+
+    // endregion - Constructors
+
+    // region - Getters and Setters
+
+    public void setObserver(ActionObserver observer){
+        this.obsevers.add(observer);
+    }
+
+    // endregion - Getters and Setters
+
+    // region - Methods for/from SuperClass/Interfaces
+
+    abstract protected ActionResult getExecutionResult();
+
+    abstract protected ActionUndoResult getExecutionUndoResult();
+
+    // endregion - Methods for/from SuperClass/Interfaces
+
+    // region - Methods
+
+    public void execute(){
+        final ActionResult result = getExecutionResult();
+
+        for (ActionObserver observer : this.obsevers){
+            observer.sendActionResult(result);
+        }
+    }
+
+    public void undo(){
+        final ActionUndoResult undoResult = getExecutionUndoResult();
+
+        for (ActionObserver observer : this.obsevers){
+            observer.sendActionUndoResult(undoResult);
+        }
+    }
+
+    // endregion - Methods
+
+    // region - Inner and Anonymous Classes
+
+    public interface IActionData {
+
+        String toJson();
 
     }
+
+    public interface ActionObserver {
+
+        void sendActionResult(ActionResult result);
+
+        void sendActionUndoResult(ActionUndoResult undoResult);
+
+    }
+
+    public interface ActionResult {
+
+    }
+
+    public interface ActionUndoResult {
+
+    }
+
+    // endregion - Inner and Anonymous Classes
 
 }
